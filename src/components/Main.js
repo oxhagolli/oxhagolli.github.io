dceab8286db90cbcc61297115edaa871395f1185
Orens Xhagolli <orens.xhagolli@hotmail.com>
2019-04-03 00:24:54 -0700

    About page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import pic01 from '../images/pic01.jpg'
-import pic02 from '../images/pic02.jpg'
-import pic03 from '../images/pic03.jpg'
-
 class Main extends React.Component {
   render() {
 
@@ -13,23 +9,26 @@ class Main extends React.Component {
     return (
       <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
 
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">About</h2>
+          <p>I was born in Pogradec, Albania and grew up in Tirana, Albania. In high school, my family and I moved to Philadelphia, PA.</p>
+          <p>I graduated with honors from Rochester Institute of Technology with a Bachelor's and Master's both in Computer Science. My undergraduate focus is in Computer Graphics, while my graduate immersion is in Intelligent Systems and Artificial Intelligence.</p>
+          <p>I enjoy working on ideas and projects in addition to my day job at Microsoft. I love playing guitar (big Red Hot Chili Peppers fan), tennis occasionally and hiking on the weekends.</p>
+          <p>Favorite computer games: CS:GO, Factorio, Software Inc., Cities: Skylines, Civ V, Tropico 4, KSP, Rome: Total War, HoI4, ...</p>
+          <p>Favorite comedians: Mark Normand, Nate Bargatze, Kevin Hart, Chris Rock, Jim Jefferies</p>
+          <p>Favorite shows: Silicon Valley, The Office, Friends</p>
           {close}
         </article>
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Work</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
           <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
           <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
           {close}
         </article>
 
         <article id="resume" className={`${this.props.article === 'resume' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Resume</h2>
           <embed className="resume" src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://github.com/oxhagolli/resume/raw/master/main.pdf"></embed>
           {close}
         </article>
