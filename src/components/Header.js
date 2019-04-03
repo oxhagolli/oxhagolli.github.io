d703e82da77f234a50a2b436aa3d6dc10eb11515
Orens Xhagolli <orens.xhagolli@hotmail.com>
2019-04-03 00:55:22 -0700

    Remove that marvel stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,10 +12,8 @@ const Header = (props) => (
             <div className="inner">
                 <h1>Orens Xhagolli</h1>
                 <p>Hi! I'm an engineer. I like to learn and build stuff! </p>
-                <p>My professional interests include Data Science + ML and startups. <br/>
-                If work was a Marvel movie, my superpower would be building end-to-end scalable ML systems.</p>
-                <p>My general interests include tennis, hiking and playing guitar. I love video games and standup comedy.<br/>
-                If life was a Marvel movie, my superpower would be that I'd explode every time something becomes routine.</p>
+                <p>My professional interests include Data Science + ML and startups.</p>
+                <p>My general interests include tennis, hiking and playing guitar. I love video games and standup comedy.</p>
             </div>
         </div>
         <nav>
