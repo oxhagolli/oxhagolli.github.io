deae87244d10b59ebfac1226353854945bdaef8b
Orens Xhagolli <orens.xhagolli@hotmail.com>
2019-04-02 22:20:49 -0700

    Add header image and fix image references

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import orens from "../images/orens.png";
+
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-diamond"></span>
+            <img className="pic" src={orens} alt="orens"/>
         </div>
         <div className="content">
             <div className="inner">
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../components/layout'
 
+import wrong from "../images/wrong.gif"
+
 const NotFoundPage = () => (
   <Layout>
     <h1>NOT FOUND!</h1>
     <p>You just hit a route that doesn&#39;t exist...</p>
-    <img src={require("../images/wrong.gif")} alt="Oops!" />
+    <img src={wrong} alt="Oops!" />
   </Layout>
 )
 
